9dddbdb59451fe4179dd7fc0932d3b0b1365a451
tom <daffautomo54@gmail.com>
2025-02-19 09:49:17 +0700

    Perubahan menjadi tabel & menambahkan tabel discount

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ function searchFunction() {
         }
     }
 }
+
+function searchFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("productTable");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 1; i < tr.length; i++) {
+        tr[i].style.display = "none";
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            }
+        }       
+    }
+}
